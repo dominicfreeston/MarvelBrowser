fa9c227f3aa22bbe753ba17b8f65cb07c4eecc74
Dominic Freeston <dominic.freeston@gmail.com>
2017-07-07 23:01:51 +0100

    Add Character type

---
+++ MarvelBrowser/Character.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+typealias CharacterIdentifier = Int
+
+struct Character {
+    let identifier: CharacterIdentifier
+    let name: String
+    let description: String
+    let thumbnail: ImageAsset
+}
